1a56e4c416cfb8ff75bd5d81ead456132b1a8929
yekta <ngyekta@gmail.com>
2022-05-17 05:07:48 +0300

    Style fixes

--- src/lib/components/Navbar.svelte
+++ src/lib/components/Navbar.svelte
@@ -40,8 +40,8 @@
 
 <nav class="w-full flex justify-center absolute top-0 left-0 right-0 text-c-primary z-50">
 	<div class="container-b max-w-full flex flex-row items-center justify-between px-4 py-3">
-		<Logo />
-		<div class="flex flex-row justify-end items-center">
+		<Logo class="text-c-primary w-48 h-auto mr-12 py-1.5" />
+		<div class="hidden md:flex min-w-0 flex-row flex-1 justify-end items-center overflow-hidden">
 			{#each sections as section}
 				<a href="/#{section.id}" class="px-4 py-2">{section.title}</a>
 			{/each}
--- src/lib/components/sections/Testimonials.svelte
+++ src/lib/components/sections/Testimonials.svelte
@@ -12,7 +12,7 @@
 			{#each testimonials as testimonial}
 				<div class="w-full md:w-1/2 mt-5 p-3">
 					<div
-						class="w-full chat-bubble bg-c-secondary filter brightness-110 shadow-card shadow-c-on-bg/6
+						class="w-full chat-bubble relative bg-c-secondary filter brightness-110 shadow-card shadow-c-on-bg/6
 						rounded-tl-xl rounded-tr-xl rounded-br-xl flex flex-col items-center text-center px-10 py-5"
 					>
 						<p>“{testimonial}”</p>
