d53abf4acf5ab5b43fd173a07259caac81548355
yekta <40706601+yekta@users.noreply.github.com>
2022-05-17 14:59:09 +0300

    Explorer section

--- src/lib/components/sections/Explorer.svelte
+++ src/lib/components/sections/Explorer.svelte
@@ -1,10 +1,17 @@
 <script lang="ts">
 	import Button from '../Button.svelte';
+
+	let inputExplorer: string;
+
+	async function handleSubmit(e: Event) {
+		window.open(`https://creeper.banano.cc/explorer/auto/${inputExplorer}`, '_blank');
+		inputExplorer = '';
+	}
 </script>
 
 <div
 	id="explorer"
-	class="w-full bg-creeper bg-c-secondary flex justify-center pt-16 pb-24 text-center text-c-bg"
+	class="w-full bg-creeper bg-c-secondary flex justify-center pt-16 pb-20 text-center text-c-bg"
 >
 	<div class="container-b flex flex-col items-center px-4 md:px-8">
 		<h2 class="text-3xl font-bold">Explorer</h2>
@@ -15,19 +22,22 @@
 				target="_blank">creeper.banano.cc</a
 			>.
 		</p>
-		<div
-			class="container-b-smallest flex flex-col items-center md:flex-row md:items-end mt-8 text-left"
+		<form
+			on:submit|preventDefault={handleSubmit}
+			class="container-b-smallest flex flex-col items-center md:flex-row md:items-end mt-8
+			text-left"
 		>
 			<label for="explorer-input" class="w-full max-w-sm md:max-w-full flex flex-col md:pr-4">
 				<p class="px-3 text-lg">Enter a Banano address or block hash below to search.</p>
 				<input
 					id="explorer-input"
+					bind:value={inputExplorer}
 					type="text"
 					class="text-c-on-bg w-full px-4 py-3.5 mt-3 rounded-lg"
 				/>
 			</label>
 			<Button class="w-full md:w-auto max-w-sm px-8 md:px-12 mt-3">Search</Button>
-		</div>
+		</form>
 	</div>
 </div>
 
