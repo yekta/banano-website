8f009f7d50d20b2aff65a30397b72ebe32760dba
yekta <ngyekta@gmail.com>
2022-05-17 18:06:11 +0300

    Style tweaks

--- content/community-projects.md
+++ content/community-projects.md
@@ -2,22 +2,22 @@
 
 A place to get help getting started.
 
-###### [banano.how](https://banano.how)
+#### [banano.how](https://banano.how)
 
 ### Banano.bet
 
 A provably fair dice game for Banano.
 
-###### [banano.bet](https://banano.bet)
+#### [banano.bet](https://banano.bet)
 
 ### JungleTv.live
 
 A video sharing platfrom where you can earn & spend Banano.
 
-###### [jungletv.live](https://jungletv.live)
+#### [jungletv.live](https://jungletv.live)
 
 ### Banano.fun
 
 Tailor-made Banano memes and animations.
 
-###### [banano.fun](https://banano.fun)
+#### [banano.fun](https://banano.fun)
--- src/lib/components/sections/CommunityProjects.svelte
+++ src/lib/components/sections/CommunityProjects.svelte
@@ -9,12 +9,12 @@
 	id="community-projects"
 	class="w-full bg-community bg-c-secondary flex justify-center pt-20 pb-28 text-center text-c-bg"
 >
-	<div class="container-b flex flex-col items-center">
+	<div class="container-b flex flex-col items-center px-4">
 		<h2 class="text-3xl font-bold">Community Projects</h2>
 		<p class="mt-2 text-xl">
 			Here are some of the projects that are made by our awesome community:
 		</p>
-		<div class="w-full flex flex-wrap justify-center">
+		<div class="w-full flex flex-wrap justify-center items-end">
 			{#each communityProjects as project}
 				<div
 					class="w-full md:w-1/2 lg:w-1/4 max-w-[17rem] flex flex-col items-center text-center px-4 mt-5"
