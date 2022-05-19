f36032d006cbd8ce1eb9dc872f5a57df319eb7d8
yekta <ngyekta@gmail.com>
2022-05-19 06:19:10 +0300

    Editor import

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -6,3 +6,12 @@
 
 <Navbar />
 <slot />
+
+<svelte:head>
+	<style>
+		body {
+			color: rgb(var(--c-on-bg));
+			background-color: rgb(var(--c-secondary));
+		}
+	</style>
+</svelte:head>
--- src/routes/_editor/index@editor.svelte
+++ src/routes/_editor/index@editor.svelte
@@ -1,35 +1,16 @@
 <script lang="ts">
-	import Button from '$lib/components/Button.svelte';
 	import { onMount } from 'svelte';
+	import { remark } from 'remark';
+	import remarkHtml from 'remark-html';
 
 	let EditorJS: any;
 	let Header: any;
-	let editor: any;
+	let editor: any = undefined;
+	let html: string;
+	let isEditorShowing = false;
 
 	let saveTimeout: NodeJS.Timeout;
 	const saveDelay = 1000;
-
-	let data = {
-		blocks: [
-			{
-				type: 'header',
-				data: {
-					text: 'This is a test',
-					level: 1
-				}
-			},
-			{
-				type: 'paragraph',
-				data: {
-					text: "I'm testing something here."
-				}
-			}
-		]
-	};
-
-	function setData() {
-		editor.render(data);
-	}
 
 	function onKeypress() {
 		clearTimeout(saveTimeout);
@@ -45,14 +26,26 @@
 		}, saveDelay);
 	}
 
-	async function getData() {
-		let res = await fetch(
-			'https://raw.githubusercontent.com/yekta/banano-website/master/content/announcements.md'
-		);
-		let text = await res.text();
+	async function setup() {
+		let [resHtml, editor] = await Promise.all([getData(), loadEditor()]);
+		editor.blocks.renderFromHTML(resHtml);
 	}
 
-	onMount(async () => {
+	async function getData() {
+		let res = await fetch(
+			'https://raw.githubusercontent.com/yekta/banano-website/master/content/team.md'
+		);
+		let text = await res.text();
+		let resHtml = await remark()
+			.use(remarkHtml)
+			.process(text)
+			.then((file) => {
+				return String(file);
+			});
+		return resHtml;
+	}
+
+	async function loadEditor() {
 		Header = (await import('@editorjs/header' as any)).default;
 		EditorJS = (await import('@editorjs/editorjs')).default;
 		editor = new EditorJS({
@@ -71,14 +64,15 @@
 				}
 			}
 		});
-		await getData();
+		await editor.isReady;
+		return editor;
+	}
+	onMount(async () => {
+		setup();
 	});
 </script>
 
 <svelte:window on:keypress={onKeypress} />
 <div class="min-h-screen bg-c-bg flex flex-col py-12">
-	<div class="w-[650px] max-w-full flex flex-row justify-start items-center mx-auto mb-8">
-		<Button onClick={setData} padding="px-12 py-2">Fill</Button>
-	</div>
 	<div id="editorjs" />
 </div>
