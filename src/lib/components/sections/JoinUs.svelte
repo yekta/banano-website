293afa6e51db5d578527d10f911b02b7e8c7e3d4
yekta <ngyekta@gmail.com>
2022-05-20 22:02:26 +0300

    Padding adjustments

--- src/lib/components/sections/JoinUs.svelte
+++ src/lib/components/sections/JoinUs.svelte
@@ -65,7 +65,7 @@
 	];
 </script>
 
-<div id="join-us" class="w-full bg-community flex justify-center py-16 text-center">
+<div id="join-us" class="w-full bg-community flex justify-center py-12 text-center">
 	<div class="container-b-small flex flex-col items-center">
 		<h2 class="text-3xl font-bold px-4">Join the Crypto-Potassium Revolution!</h2>
 		<div class="w-full flex flex-wrap justify-center mt-2 px-4 md:px-8">
