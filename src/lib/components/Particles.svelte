fc5d64bb904a73be90bf40ee9bb957d821ff08df
yekta <ngyekta@gmail.com>
2022-05-25 09:06:04 +0300

    Particles

--- src/lib/components/Particles.svelte
+++ src/lib/components/Particles.svelte
@@ -1,107 +1,91 @@
 <script lang="ts">
-	import { tick } from 'svelte';
+	import { onMount } from 'svelte';
 
 	interface IParticle {
 		startX: number;
 		startY: number;
 		endX: number;
 		endY: number;
-		scale: number;
+		currentX: number;
+		currentY: number;
 		speed: number;
 		distance: number;
 		birth: number;
+		duration: number;
+		scaledWidth: number;
+		scaledHeight: number;
 	}
 
 	let particles: IParticle[];
 	let containerWidth: number;
 	let containerHeight: number;
 	let context: any;
-
-	const animDurationMultiplier = 13;
-
 	let canvas: HTMLCanvasElement;
 
-	let renderCanvas = false;
-	$: if (containerWidth !== undefined && containerHeight !== undefined) {
-		renderCanvas = true;
-	}
+	const animDurationMultiplier = 15;
+	const lineThreshold = 300;
+	const strokeColor = '#fbdd11';
+	const particleCount = 30;
 
-	$: if (renderCanvas) startAnimating();
-
-	let imgLoaded = false;
 	let img: HTMLImageElement;
+	let imgLoaded = false;
 
-	async function startAnimating() {
-		await tick();
-		img = new Image();
-		img.src = '/images/banana-for-particles.png';
-		img.onload = () => {
-			imgLoaded = true;
-		};
-		particles = Array.from(Array(40), (_, i) => {
-			let randFlip = Math.round(Math.random());
-			let randSpeedAndScale = Math.random();
-			let x1: number;
-			let y1: number;
-			let x2: number;
-			let y2: number;
-			x1 = Math.random() * containerWidth;
-			y1 = Math.random() * containerHeight;
-			if (randFlip === 0) {
-				x2 = Math.round(Math.random()) * containerWidth;
-				y2 = Math.random() * containerHeight;
-			} else {
-				y2 = Math.round(Math.random()) * containerHeight;
-				x2 = Math.random() * containerWidth;
-			}
-			let distance = Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
-			return {
-				startX: x1,
-				startY: y1,
-				endX: x2,
-				endY: y2,
-				scale: randSpeedAndScale * 0.5 + 0.1,
-				speed: 1,
-				distance: distance,
-				birth: Date.now()
-			};
-		});
-	}
-	$: if (imgLoaded) init();
+	$: if (imgLoaded && canvas) init();
 
 	function init() {
 		context = canvas.getContext('2d');
+		context.strokeStyle = strokeColor;
+		context.lineWidth = 0.5;
 		window.requestAnimationFrame(draw);
 	}
 
-	function createNewParticle(): IParticle {
+	function createNewParticle(isInitial = false): IParticle {
 		let randFlip = Math.round(Math.random());
 		let randSpeedAndScale = Math.random();
 		let x1: number;
 		let y1: number;
 		let x2: number;
 		let y2: number;
-		if (randFlip === 0) {
-			x1 = Math.round(Math.random()) * containerWidth;
-			x2 = containerWidth - x1;
-			y1 = Math.random() * containerHeight;
-			y2 = Math.random() * containerHeight;
+		if (isInitial) {
+			x1 = randomizedX();
+			y1 = randomizedY();
+			if (randFlip === 0) {
+				x2 = Math.random() > 0.5 ? maxX() : minX();
+				y2 = randomizedY();
+			} else {
+				y2 = Math.random() > 0.5 ? maxY() : minY();
+				x2 = randomizedX();
+			}
 		} else {
-			y1 = Math.round(Math.random()) * containerHeight;
-			y2 = containerHeight - y1;
-			x1 = Math.random() * containerWidth;
-			x2 = Math.random() * containerWidth;
+			if (randFlip === 0) {
+				x1 = Math.random() > 0.5 ? maxX() : minX();
+				x2 = maxX() - x1;
+				y1 = randomizedY();
+				y2 = randomizedY();
+			} else {
+				y1 = Math.random() > 0.5 ? maxY() : minY();
+				y2 = maxY() - y1;
+				x1 = randomizedX();
+				x2 = randomizedX();
+			}
 		}
-		let distance = Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
+
+		const distance = Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
+		const speed = 1;
+		const scale = randSpeedAndScale * 0.6 + 0.1;
 		return {
 			startX: x1,
 			startY: y1,
 			endX: x2,
 			endY: y2,
-			scale: randSpeedAndScale * 0.5 + 0.1,
-			speed: 1,
+			currentX: x1,
+			currentY: y1,
+			scaledWidth: scale * img.width,
+			scaledHeight: scale * img.height,
+			speed: speed,
 			distance: distance,
-			birth: Date.now()
+			birth: Date.now(),
+			duration: (distance / speed) * animDurationMultiplier
 		};
 	}
 
@@ -112,14 +96,15 @@
 			containerWidth * 2,
 			containerHeight * 2
 		);
+		let milliseconds = Date.now();
 		for (let i = 0; i < particles.length; i++) {
 			let particle = particles[i];
-			let milliseconds = Date.now();
 			let diff = milliseconds - particle.birth;
-			let duration = (particle.distance / particle.speed) * animDurationMultiplier;
-			let currentX = particle.startX + (particle.endX - particle.startX) * (diff / duration);
-			let currentY = particle.startY + (particle.endY - particle.startY) * (diff / duration);
-			if (currentX < 0 || currentX > containerWidth || currentY < 0 || currentY > containerHeight) {
+			let currentX =
+				particle.startX + (particle.endX - particle.startX) * (diff / particle.duration);
+			let currentY =
+				particle.startY + (particle.endY - particle.startY) * (diff / particle.duration);
+			if (currentX < minX() || currentX > maxX() || currentY < minY() || currentY > maxY()) {
 				particles.splice(i, 1);
 				particles.push(createNewParticle());
 			} else {
@@ -131,13 +116,55 @@
 					img.height,
 					currentX,
 					currentY,
-					img.width * particle.scale,
-					img.height * particle.scale
+					particle.scaledWidth,
+					particle.scaledHeight
 				);
+				particle.currentX = currentX;
+				particle.currentY = currentY;
+				for (let y = i; y > 0; y--) {
+					let otherParticle = particles[y];
+					let distance = Math.sqrt(
+						Math.pow(currentX - otherParticle.currentX, 2) +
+							Math.pow(currentY - otherParticle.currentY, 2)
+					);
+					if (distance < lineThreshold) {
+						context?.beginPath();
+						context?.moveTo(
+							currentX + particle.scaledWidth / 2,
+							currentY + particle.scaledHeight / 2
+						);
+						context?.lineTo(
+							otherParticle.currentX + otherParticle.scaledWidth / 2,
+							otherParticle.currentY + otherParticle.scaledHeight / 2
+						);
+						let opacity = 1 - distance / lineThreshold;
+						context.strokeStyle = `rgba(251,221,16,${opacity})`;
+						context?.stroke();
+					}
+				}
 			}
 		}
 		window.requestAnimationFrame(draw);
 	}
+
+	const minX = () => -1 * img.width;
+	const maxX = () => containerWidth + img.width;
+	const minY = () => -1 * img.height;
+	const maxY = () => containerHeight + img.height;
+
+	const randomizedX = () => random(minX(), maxX());
+	const randomizedY = () => random(minY(), maxY());
+
+	const random = (min: number, max: number) => Math.random() * (max - min) + min;
+
+	onMount(() => {
+		img = new Image();
+		img.src = '/images/banana-for-particles.png';
+		img.onload = () => {
+			imgLoaded = true;
+			particles = Array.from(Array(particleCount), (_, i) => createNewParticle(true));
+		};
+	});
 </script>
 
 <div
@@ -145,7 +172,7 @@
 	bind:clientHeight={containerHeight}
 	class="w-full h-full absolute left-0 top-0 overflow-hidden"
 >
-	{#if renderCanvas}
+	{#if containerWidth !== undefined && containerHeight !== undefined}
 		<canvas bind:this={canvas} width={containerWidth} height={containerHeight} />
 	{/if}
 </div>
