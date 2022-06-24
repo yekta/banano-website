23269e923a50241e9d19607d9a764deda85d51de
yekta <ngyekta@gmail.com>
2022-06-25 02:53:12 +0300

    Robot.txt and sitemap.xml

---
+++ src/routes/sitemap.xml.ts
@@ -0,0 +1,117 @@
+import { canonicalUrl } from '$lib/ts/constants/canonical';
+import type { RequestHandler } from '@sveltejs/kit';
+
+export const get: RequestHandler = async (event) => {
+	const allBlogRoutes = await getBlogRoutesArray();
+	const blogRoutes = allBlogRoutes.slice(1);
+	const allRoutes = [...definedRoutes, ...blogRoutes];
+	const headers = {
+		'Cache-Control': `max-age=0, s-max-age=${600}`,
+		'Content-Type': 'text/xml'
+	};
+	const body = render(allRoutes);
+	return {
+		status: 200,
+		headers,
+		body
+	};
+};
+
+function render(routes: IRoute[]) {
+	const xml = `<?xml version="1.0" encoding="UTF-8"?>
+    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+    	${routes
+				.map(
+					(route) =>
+						`<url><loc>${canonicalUrl}${route.loc}</loc><lastmod>${route.lastmod}</lastmod><changefreq>${route.changefreq}</changefreq></url>`
+				)
+				.join('')}
+    </urlset>`;
+	return xml;
+}
+
+async function getBlogRoutesArray() {
+	const routes: IRoute[] = [];
+	try {
+		const res = await fetch(url);
+		const resJson = await res.json();
+		const posts: IPost[] = resJson.posts;
+		const blogRoutes = posts.map((p) => {
+			let date = new Date(p.updated_at);
+			let year = date.getFullYear();
+			let month = date.getMonth() + 1;
+			let day = date.getDate();
+			let dateString = `${year}-${month >= 10 ? month : '0' + month}-${
+				day >= 10 ? day : '0' + day
+			}`;
+			let route: IRoute = {
+				loc: `${blogDirectory}/${p.slug}`,
+				lastmod: dateString,
+				changefreq: 'daily'
+			};
+			return route;
+		});
+		routes.push(...blogRoutes);
+	} catch (error) {
+		console.log(error);
+	}
+	return routes;
+}
+
+const blogApiUrl = 'https://ghost.banano.cc/ghost/api/content';
+const blogApiKey = String(import.meta.env.VITE_GHOST_KEY);
+const shallowPostFields = ['slug', 'updated_at'];
+
+const url = `${blogApiUrl}/posts?key=${blogApiKey}&fields=${shallowPostFields.join(',')}&limit=500`;
+const blogDirectory = '/blog';
+
+const today = new Date();
+const year = today.getFullYear();
+const month = today.getMonth() + 1;
+const day = today.getDate();
+const todayString = `${year}-${month >= 10 ? month : '0' + month}-${day >= 10 ? day : '0' + day}`;
+
+const definedRoutes: IRoute[] = [
+	{
+		loc: '/',
+		lastmod: todayString,
+		changefreq: 'daily'
+	},
+	{
+		loc: '/blog',
+		lastmod: todayString,
+		changefreq: 'daily'
+	},
+	{
+		loc: '/paperwallet',
+		lastmod: todayString,
+		changefreq: 'daily'
+	},
+	{
+		loc: '/yellowpaper',
+		lastmod: todayString,
+		changefreq: 'daily'
+	},
+	{
+		loc: '/redeem',
+		lastmod: todayString,
+		changefreq: 'daily'
+	},
+	{
+		loc: '/presskit',
+		lastmod: todayString,
+		changefreq: 'daily'
+	}
+];
+
+interface IRoute {
+	loc: string;
+	lastmod: string;
+	changefreq: string;
+}
+
+interface IPost {
+	id: string;
+	slug: string;
+	updated_at: string;
+}
--- utils/blogRoutes.js
+++ utils/blogRoutes.js
@@ -1,7 +1,6 @@
 import axios from 'axios';
 import axiosRetry from 'axios-retry';
 import 'dotenv/config';
-/* import fs from 'fs';*/
 
 axiosRetry(axios, {
 	retries: 5,
@@ -12,7 +11,7 @@ axiosRetry(axios, {
 
 const blogApiUrl = 'https://ghost.banano.cc/ghost/api/content';
 const blogApiKey = process.env.VITE_GHOST_KEY;
-const shallowPostFields = ['id', 'uuid', 'title', 'slug'];
+const shallowPostFields = ['id', 'slug'];
 
 const url = `${blogApiUrl}/posts?key=${blogApiKey}&fields=${shallowPostFields.join(',')}&limit=500`;
 const blogDirectory = '/blog';
@@ -25,13 +24,6 @@ export async function getBlogRoutesArray() {
 		const routes = resJson.posts.map((p) => `${blogDirectory}/${p.slug}`);
 		pages.push(...routes);
 		console.log('Pages to fetch:', pages);
-
-		/* const slugFile = `./post-slugs.txt`;
-		const slugFileContent = resJson.posts.map((i) => `https://banano.cc/blog/${i.slug}`).join('\n');
-		fs.writeFile(slugFile, slugFileContent, function (err) {
-			console.log(err);
-		});
-		console.log('Slug file created:', slugFile); */
 	} catch (error) {
 		console.log(error);
 	}
