03ba08fb8a9cd84cfcda8eb255a7ca1c674074f2
yekta <40706601+yekta@users.noreply.github.com>
2022-07-18 04:28:03 +0300

    Bform background

--- src/routes/bform/index@nofooter.svelte
+++ src/routes/bform/index@nofooter.svelte
@@ -91,7 +91,11 @@
 		: innerHeight + 'px'};"
 	class="w-full bg-cover bg-c-secondary flex flex-col items-center justify-start text-center"
 >
-	<div class="w-full flex justify-center pt-24 pb-32 px-3 md:px-12 my-auto">
+	<div
+		style="background:linear-gradient(0deg, rgb(17,51,13, 0) 50%, rgba(17,51,13, 0.3) 100%);"
+		class="w-full h-full absolute left-0 top-0 pointer-events-none"
+	/>
+	<div class="w-full flex justify-center pt-24 pb-32 px-3 md:px-12 my-auto relative">
 		<div
 			class="w-full max-w-lg md:max-w-3xl bg-c-bg rounded-2xl md:rounded-3xl 
 			shadow-button shadow-c-bg-secondary-shaded overflow-hidden relative z-0 transition"
